fadf27468c581388b144e2dd4679bd23a2c6970a
Wangsamuels <128024773+Wangsamuels@users.noreply.github.com>
2024-08-21 12:50:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ gdown.download(url, output, quiet=True)
 
 #Tensorflow model prediction
 def model_prediction(test_image):
-    model = tf.keras.models.load_model(output, compile=False)
+    model = tf.keras.models.load_model(output)
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(64, 64))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])

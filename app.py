f0d1054ddd72346c600a03024966c48a5b9f4ae2
Wangsamuels <128024773+Wangsamuels@users.noreply.github.com>
2024-08-24 05:05:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,26 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-import gdown
-import requests
 
 
-def download_file_from_google_drive(url, output_path):
-    try:
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open(output_path, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-        st.success(f"Downloaded {output_path}")
-        return output_path
-    except requests.exceptions.RequestException as e:
-        st.error(f"Download failed: {e}")
-        return None
-
-# URL and output file names
-url_weights = "https://drive.google.com/uc?id=171x-xwdqm2dXMOyZiCfPwY6xU6IWFGyF"
-output_weights = "model.weights.h5"
-
-# Download weights
-download_file_from_google_drive(url_weights, output_weights)
-
-# Check if the weights file exists
-st.write(f"Path to weights file: {output_weights}")
-
-# Load the weights into the model
-try:
-    model.load_weights(output_weights)
-    st.success("Model weights loaded successfully.")
-except Exception as e:
-    st.error(f"Error loading weights: {e}")
-# TensorFlow model prediction function
-
+#Tensorflow Model Prediction
 def model_prediction(test_image):
-    image = tf.keras.preprocessing.image.load_img(test_image, target_size=(64, 64))
+    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    input_arr = np.expand_dims(input_arr, axis=0)  # Add batch dimension
-    prediction = model.predict(input_arr)
-    result_index = np.argmax(prediction)
-    return result_index
+    input_arr = np.array([input_arr]) #convert single image to batch
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions) #return index of max element
 
+#Sidebar
 st.sidebar.title("Dashboard")
-app_mode = st.sidebar.selectbox("Select", ["Home", "About", "Disease Recognition"])
+app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recognition"])
 
-#HOME PAGE
+#Main Page
 if(app_mode=="Home"):
-    st.header("PLANT DISEASE RECOGNITION AI")
-    image_path = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fphotos%2Fplant.html&psig=AOvVaw3S2S-OhYERARbmg9cp9rzS&ust=1724320278447000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOietuHnhYgDFQAAAAAdAAAAABAE"
-    st.image(image_path, use_column_width=True)
+    st.header("PLANT DISEASE RECOGNITION SYSTEM")
+    image_path = "home_page.jpeg"
+    st.image(image_path,use_column_width=True)
     st.markdown("""
     Welcome to the Plant Disease Recognition System! 🌿🔍
     
@@ -72,11 +41,10 @@ if(app_mode=="Home"):
 
     ### About Us
     Learn more about the project, our team, and our goals on the **About** page.
-    """
-    )
+    """)
 
-#ABOUT PAGE
-elif(app_mode=='About'):
+#About Project
+elif(app_mode=="About"):
     st.header("About")
     st.markdown("""
                 #### About Dataset
@@ -88,56 +56,32 @@ elif(app_mode=='About'):
                 2. test (33 images)
                 3. validation (17572 images)
 
-                """
-    )    
+                """)
 
-#PREDICTION PAGE
-elif(app_mode=='Disease Recognition'):
-    st.header("Disease Recognition")  
-    test_image = st.file_uploader("Choose an image")
+#Prediction Page
+elif(app_mode=="Disease Recognition"):
+    st.header("Disease Recognition")
+    test_image = st.file_uploader("Choose an Image:")
     if(st.button("Show Image")):
-        st.image(test_image, use_column_width=True)  
+        st.image(test_image,width=4,use_column_width=True)
+    #Predict button
     if(st.button("Predict")):
-        st.balloons()
+        st.snow()
         st.write("Our Prediction")
         result_index = model_prediction(test_image)
-        #Define class
-        class_name = ['Apple___Apple_scab',
- 'Apple___Black_rot',
- 'Apple___Cedar_apple_rust',
- 'Apple___healthy',
- 'Blueberry___healthy',
- 'Cherry_(including_sour)___Powdery_mildew',
- 'Cherry_(including_sour)___healthy',
- 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
- 'Corn_(maize)___Common_rust_',
- 'Corn_(maize)___Northern_Leaf_Blight',
- 'Corn_(maize)___healthy',
- 'Grape___Black_rot',
- 'Grape___Esca_(Black_Measles)',
- 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
- 'Grape___healthy',
- 'Orange___Haunglongbing_(Citrus_greening)',
- 'Peach___Bacterial_spot',
- 'Peach___healthy',
- 'Pepper,_bell___Bacterial_spot',
- 'Pepper,_bell___healthy',
- 'Potato___Early_blight',
- 'Potato___Late_blight',
- 'Potato___healthy',
- 'Raspberry___healthy',
- 'Soybean___healthy',
- 'Squash___Powdery_mildew',
- 'Strawberry___Leaf_scorch',
- 'Strawberry___healthy',
- 'Tomato___Bacterial_spot',
- 'Tomato___Early_blight',
- 'Tomato___Late_blight',
- 'Tomato___Leaf_Mold',
- 'Tomato___Septoria_leaf_spot',
- 'Tomato___Spider_mites Two-spotted_spider_mite',
- 'Tomato___Target_Spot',
- 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
- 'Tomato___Tomato_mosaic_virus',
- 'Tomato___healthy']
-        st.success("Model predicts that, it's a {}".format(class_name[result_index]))
+        #Reading Labels
+        class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
+                    'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 
+                    'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 
+                    'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 
+                    'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 
+                    'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
+                    'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 
+                    'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 
+                    'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 
+                    'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 
+                    'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 
+                    'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 
+                    'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
+                      'Tomato___healthy']
+        st.success("Model is Predicting it's a {}".format(class_name[result_index]))
